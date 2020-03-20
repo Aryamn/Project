9d7b09559e36ae932b02351f96932388872adb29
Aryaman <jainaryaman123@gmail.com>
2020-03-21 02:10:27 +0530

    database user auth

--- lecyc/form.py
+++ lecyc/form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo ,ValidationError
+from lecyc.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,6 +13,22 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self,username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError("Username is taken . Please choose another")
+            # print("Username is taken . Please choose another")
+
+    def validate_email(self,email):
+
+        error = User.query.filter_by(email=email.data).first()
+
+        if error:
+            raise ValidationError("Email is taken . Please choose another")
+            # print("Username is taken . Please choose another")
+            
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
--- lecyc/models.py
+++ lecyc/models.py
@@ -1,15 +1,20 @@
-from lecyc import db
+from lecyc import db , login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model , UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120) , unique = True , nullable = False)
     image_file = db.Column(db.String(20), nullable=False,
                            default='default.png')
-    hall = db.Column(db.String(100), nullable=False)
-    roll_no = db.Column(db.String(10), unique=True, nullable=False)
-    mobile_no = db.Column(db.Integer, unique=True, nullable=False)
+    # hall = db.Column(db.String(100), nullable=False)
+    # roll_no = db.Column(db.String(10), unique=True, nullable=False)
+    # mobile_no = db.Column(db.Integer, unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     cycles = db.relationship('Cycle', backref='author', lazy=True)
 
